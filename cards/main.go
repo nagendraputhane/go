71d9374a6fa8ede6d4627d8ed26cf6c7c6e36cbe
Nagendra T P <ntp@marvell.com>
2023-05-07 22:52:58 +0530

    saveToFile

--- cards/main.go
+++ cards/main.go
@@ -3,18 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//array - fixed. Same datatype
-	//slice - array that can grown and shrink
-	cards := []string{"Ace of Diamonds", newCard()}
-	//var = []datatype{}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		//for index, current item := range slice {}
-		fmt.Println(i, card)
-	}
-}
+	hand, remainingCards := deal(cards, 5)
 
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
+
+	fmt.Println(cards.toString())
+
+	cards.saveToFile("my_cards")
 }
